aaf3774a85ebe83091345960cb3e09b2558babc8
vagrant <vagrant@rails-dev-box>
2013-12-03 05:12:45 +0000

    Installed Faker and seeded the database. Added as acts-as-taggable-on gem.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+rand(10..30).times do
+  p = Bookmark.create(title: Faker::Lorem.words(rand(1..10)).join(" "), burl: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+end
+
+puts "Seed finished"
+puts "#{Bookmark.count} bookmarks created"
