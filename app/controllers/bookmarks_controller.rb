dc27791a7e583c5c6039b0910b9d3c2581fb45fd
vagrant <vagrant@rails-dev-box>
2013-12-04 06:25:57 +0000

    Added create and update CRUD actions.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,8 +9,33 @@ class BookmarksController < ApplicationController
 
 
   def new
+    @bookmark = Bookmark.new
   end
 
+  def create
+  @bookmark = Bookmark.new(params[:bookmark])
+  
+  if @bookmark.save
+    flash[:notice] = "Bookmark was saved."
+    redirect_to @bookmark
+  else
+    flash[:error] = "There was an error saving the bookmark. Please try again."
+    render :new
+  end
+end
+
   def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update_attributes(params[:post])
+      flash[:notice] = "Post was updated."
+      redirect_to @bookmark
+    else
+      flash[:error] = "There was an error saving the bookmark. Please try again."
+      render :edit
+    end
   end
 end
