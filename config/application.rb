8d9f737767043353106905bd5dd094ee877ce819
vagrant <vagrant@rails-dev-box>
2013-12-05 01:29:16 +0000

    styled bookmarks

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-config.assets.initialize_on_precompile = false
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -19,6 +17,8 @@ end
 
 module Brittmark
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
