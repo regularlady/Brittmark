0c6b055cb0aa8ea7874730fdd1fd48306ea17f96
vagrant <vagrant@rails-dev-box>
2013-12-16 01:55:10 +0000

    Implemented Facebook Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Brittmark::Application.routes.draw do
   resources :user_bookmarks
 
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :bookmarks
 
