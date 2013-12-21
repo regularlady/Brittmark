1b5b722249958ae31f6ce892bd2f1d7653931914
vagrant <vagrant@rails-dev-box>
2013-12-21 04:38:58 +0000

    Adjusted parsing

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,5 @@
 class EmailProcessor
   def self.process(email)
-    Bookmark.create!({ tag_list: email.body, burl: email.subject })
+    Bookmark.create!({ tag_list: email.body, burl: email.subject, title: email.body })
   end
 end
